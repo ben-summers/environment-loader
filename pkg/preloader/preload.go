a0ab8ff037901691d24fdd4b1dcb9c2e756d48c0
Ben Summers <ben@bondine.io>
2022-01-23 16:58:36 -0600

    Updated logging

--- pkg/preloader/preload.go
+++ pkg/preloader/preload.go
@@ -10,61 +10,66 @@ import (
 )
 
 const (
-	EnvironmentVariableName    = "ENVIRONMENT_PRELOADER_FILE"
-	EnvironmentPreloaderPrefix = "ENVIRONMENT_PRELOADER_PREFIX"
+	EnvironmentVariableName     = "ENVIRONMENT_PRELOADER_FILE"
+	EnvironmentPreloaderPrefix  = "ENVIRONMENT_PRELOADER_PREFIX"
+	EnvironmentPreloaderVerbose = "ENVIRONMENT_PRELOADER_VERBOSE"
 )
 
 // PreloadEnvironment looks for a file specified by an environment variable named ENVIRONMENT_PRELOADER
 func PreloadEnvironment() (string, error) {
 	var environmentPreloaderPrefix string
+	verbose := false
+
 	envFile := os.Getenv(EnvironmentVariableName)
 	if envFile != "" {
-		log.Printf("Loading environment preload file from: %s", envFile)
+		log.Printf("loading environment file: %s", envFile)
 		if _, stat := os.Stat(envFile); !os.IsNotExist(stat) {
 			if file, err := os.Open(envFile); err != nil {
 				return "", errors.New(fmt.Sprintf("Could not open environment loader file: %s, %v", envFile, err))
 			} else {
 				scanner := bufio.NewScanner(file)
-				firstScan := true
 
-			scanning:
+				// Try to find lines relevant to the preloader first.
 				for scanner.Scan() {
-					if firstScan {
-						log.Printf("Scanning first line. It should look something like %s=%s", EnvironmentPreloaderPrefix, "SOME_PREFIX")
-						line := scanner.Text()
-						chunks := strings.SplitN(line, "=", 2)
-						if len(chunks) == 2 {
-							if EnvironmentPreloaderPrefix == chunks[0] {
-								environmentPreloaderPrefix = chunks[1]
-							} else {
-								log.Printf("Did not find %s on the first line of %s. That's OK, but you might want to check on this in a local development environment.", EnvironmentPreloaderPrefix, envFile)
-								log.Println("Exiting environment preload script.")
-								break scanning
-							}
-							if environmentPreloaderPrefix != "" {
-								log.Printf("Found environment preloader prefix: %s", environmentPreloaderPrefix)
-							}
-						}
-						firstScan = false
-					} else {
-						line := scanner.Text()
-						chunks := strings.SplitN(line, "=", 2)
-						if len(chunks) == 2 {
-							prefixedName := fmt.Sprintf("%s_%s", environmentPreloaderPrefix, chunks[0])
-
-							log.Printf("Setting %s", prefixedName)
-
-							if setenvErr := os.Setenv(prefixedName, chunks[1]); setenvErr != nil {
-								return "", errors.New(fmt.Sprintf("error setting environment variable: %s => %v", chunks[0], setenvErr))
+					line := scanner.Text()
+					chunks := strings.SplitN(line, "=", 2)
+					if len(chunks) == 2 {
+						if EnvironmentPreloaderPrefix == chunks[0] {
+							environmentPreloaderPrefix = chunks[1]
+						} else if EnvironmentPreloaderVerbose == chunks[0] {
+							value := strings.ToLower(chunks[1])
+							if "yes" == value || "true" == value {
+								verbose = true
 							}
 						}
 					}
+				}
 
+				for scanner.Scan() {
+					line := scanner.Text()
+					chunks := strings.SplitN(line, "=", 2)
+					if len(chunks) == 2 {
+						if chunks[0] == EnvironmentPreloaderVerbose || chunks[0] == EnvironmentPreloaderPrefix {
+							continue
+						}
+
+						prefixedName := fmt.Sprintf("%s_%s", environmentPreloaderPrefix, chunks[0])
+
+						if verbose {
+							log.Printf("Setting %s", prefixedName)
+						}
+
+						if setenvErr := os.Setenv(prefixedName, chunks[1]); setenvErr != nil {
+							return "", errors.New(fmt.Sprintf("error setting environment variable: %s => %v", chunks[0], setenvErr))
+						}
+					}
 				}
 				if err := file.Close(); err != nil {
 					return "", err
 				}
-				log.Println("All done setting prefixed environment variables from your file. Have a nice day!")
+				if verbose {
+					log.Println("All done setting prefixed environment variables from your file. Have a nice day!")
+				}
 			}
 		}
 	}
